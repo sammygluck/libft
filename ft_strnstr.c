265b126b12b5987bb76214464a558c4b50831204
Sammy <infogls@yahoo.com>
2023-01-17 01:16:54 -0500

    before pull

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -22,5 +22,24 @@ int	main(void)
 
 char *strnstr(char *big, const char *little, size_t len)
 {
-	
+	size_t	i;
+	size_t	j;
+
+
+	if (!big)
+		return(NULL);
+	if (!little || !little[0])
+		return((char *)(big);
+	i = 0;
+	while(big[i] && i < len)
+	{
+		j = 0;
+		while(big[i + j] == little[j] && little[j] && big[i + j] && ((i + j) < len))
+			j++;
+		if (!little[j])
+			return((char *)(big + i));
+		i++;
+	}
+	return (NULL);
+
 }
