09abfd0b6f14559a7b82abf15a9d8c7b9fb2357f
Samuel Gluck <sgluck@fu-r6-p1.s19.be>
2023-01-19 12:05:07 +0100

    libft.h norminette adjusted jan 19 12:04

--- libc/libft.h
+++ libc/libft.h
@@ -6,7 +6,7 @@
 /*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:28:42 by sgluck            #+#    #+#             */
-/*   Updated: 2023/01/18 20:00:43 by sgluck           ###   ########.fr       */
+/*   Updated: 2023/01/19 12:04:12 by sgluck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,28 @@
 # include <stddef.h>
 # include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_strlen(char	*str);
 void	*ft_memset(void *str, int c, size_t len);
 void	*ft_bzero(void *str, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*memchr(const void *s, int c, size_t n);
+void	*calloc(size_t nmemb, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	strlcat(char *dst, const char *src, size_t dstsize);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
 char	*ft_strchr(char *s, int c);
 char	*ft_strrchr(char *str, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*strnstr(char *big, const char *little, size_t len);
-int	ft_atoi(char *str);
-void	*calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
-
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char	*str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(char *str);
 
 #endif
-
