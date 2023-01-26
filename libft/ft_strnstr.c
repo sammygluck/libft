520ee5d7e8329e36909edfa6a309268d564cc0de
Sammy <infogls@yahoo.com>
2023-01-26 11:58:03 -0500

    jan 26/01/2023

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,29 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 17:33:05 by sgluck            #+#    #+#             */
+/*   Updated: 2023/01/25 19:27:09 by sgluck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	unsigned int		i;
-	unsigned int		j;
+	size_t	i;
+	size_t	j;
 
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	if (!haystack && !len)
+	if (!big && !len)
+		return (NULL);
+	if (little[0] == '\0')
+		return ((char *)big);
+	//if (ft_strlen(big) == 0 && ft_strlen(little) == 0)
+	//	return ("");
+	if (!len)
 		return (NULL);
 	i = 0;
-	while (haystack[i] && i < len)
+	while (big[i] && i < len)
 	{
 		j = 0;
-		if (haystack[i] == needle[j])
-		{
-			while (i + j < len && haystack[i + j] == needle[j])
-			{
-				j++;
-				if (!needle[j])
-					return ((char *)&haystack[i]);
-			}
-		}
+		while (big[i + j] == little[j] && little[j] && big[i + j]
+			&& ((i + j) < len))
+			j++;
+		if (!little[j])
+			return ((char *)(big + i));
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
-
