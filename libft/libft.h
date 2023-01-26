0c4230c4e9473050ec6173b24aaad63b2babbc5e
Sammy <infogls@yahoo.com>
2023-01-25 23:23:45 -0500

    jan 25 20:23

--- libft/libft.h
+++ libft/libft.h
@@ -50,6 +50,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_atoi(char *str);
+int		ft_atoi(const char *str);
 
 #endif
